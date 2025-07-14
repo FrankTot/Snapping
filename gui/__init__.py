ccd5a6b891eb67978bd065a9dab1b61966e04299
FrankTot <francesco.totaro@edu-its.it>
2025-07-14 12:02:54 +0200

    Update finale e commentato __init__.py

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,1 +1,14 @@
-print("Modulo GUI caricato")
+'''
+Autore: Francesco Totaro 
+Data: 18/07/2025
+Titolo: Progetto Esame Finale 
+'''
+
+##
+## Programma principale
+## Questo modulo stampa un messaggio quando viene importato,
+## utile per il tracciamento del caricamento del modulo GUI.
+##
+
+# Sezione di output
+print("Modulo GUI caricato")  # Messaggio visualizzato quando il modulo 'gui' viene importato
