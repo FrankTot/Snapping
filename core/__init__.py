22dc92cbdc04e66ba5ae2bd06caa1596d7ec7002
FrankTot <francesco.totaro@edu-its.it>
2025-07-14 12:00:01 +0200

    Update finale e commentato __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,2 +1,14 @@
-# Stampa un messaggio quando il modulo 'core' viene importato
-print("Modulo Core caricato")
+'''
+Autore: Francesco Totaro 
+Data: 18/07/2025
+Titolo: Progetto Esame Finale 
+'''
+
+##
+## Programma principale
+## Questo modulo stampa un messaggio quando viene importato,
+## utile per tracciare il caricamento del modulo durante l'esecuzione.
+##
+
+# Sezione di output
+print("Modulo Core caricato")  # Messaggio visualizzato quando il modulo 'core' viene importato
